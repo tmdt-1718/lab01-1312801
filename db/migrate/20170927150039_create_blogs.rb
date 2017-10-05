477bcfc3d8d39cbcf2b92b3dbccc158d01be7a0e
1312801 <1312801@student.hcmus.edu.vn>
2017-10-05 20:50:35 +0700

    Edit blog schema

--- db/migrate/20170927150039_create_blogs.rb
+++ db/migrate/20170927150039_create_blogs.rb
@@ -3,10 +3,10 @@ class CreateBlogs < ActiveRecord::Migration[5.1]
     create_table :blogs do |t|
       t.string :title
       t.text :body
-      t.references :user, foreign_key: true
+      t.references :user, index: true
 
       t.timestamps
     end
-    add_index :blogs, [:user_id, :created_at]
+     add_foreign_key :books, :user
   end
 end
---
+++ db/migrate/20171005134434_create_user_blogs.rb
@@ -0,0 +1,7 @@
+class CreateUserBlogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :user_blogs do |t|
+    	
+    end
+  end
+end
