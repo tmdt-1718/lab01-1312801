bf00d9b211beeeba03e732dc78efb431701f5222
1312801 <1312801@student.hcmus.edu.vn>
2017-10-05 17:58:18 +0700

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,14 @@
 class CommentsController < ApplicationController
   def index
+  	@comments=Comment.all
+  end
+
+  def new
+@comment=Comment.all
+  end
+  def show
+  	@comment=Comment.find(params[:id])
+  end
+  def edit
   end
 end
